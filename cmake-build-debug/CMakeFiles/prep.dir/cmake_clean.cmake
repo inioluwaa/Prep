e155e6249e339f82159414a314b76f5843ee6bca
Ini Akinleye <inioluwaakinleye@gmail.com>
2019-07-05 07:16:34 +0100

    init commit

---
+++ cmake-build-debug/CMakeFiles/prep.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prep.dir/main.cpp.obj"
+  "prep.pdb"
+  "prep.exe"
+  "prep.exe.manifest"
+  "prep.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prep.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/prep.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for prep.
+# This may be replaced when dependencies are built.
