92ecbd0886c7931604047e7541afb21c92de97dc
Ini Akinleye <inioluwaakinleye@gmail.com>
2019-07-08 17:26:48 +0100

    edit ReverseOnlyLetters.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 string ReverseOnlyLetters (string S) {
     string result = S;
     int i = S.size() - 1, j = 0;
-    while (i >= 0 && j <= S.size()) {
+    while (i >= 0 && j < S.size()) {
         if (isalpha(S[i]) && isalpha(S[j])) {
             result[j] = S[i];
             j++;
