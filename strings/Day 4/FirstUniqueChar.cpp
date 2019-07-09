565dc5f2778e7ecf550b832b2998ccb7d7312ad5
Ini Akinleye <inioluwaakinleye@gmail.com>
2019-07-09 10:13:22 +0100

    add Day 4 and FirstUniqueChar.cpp

--- strings/Day 4/FirstUniqueChar.cpp
+++ strings/Day 4/FirstUniqueChar.cpp
@@ -1,3 +1,19 @@
+/*
+387. First Unique Character in a String     *EASY*
+
+Given a string, find the first non-repeating character in it and return it's index.
+If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters.
+*/
+
 #include <iostream>
 #include <string>
 #include <unordered_map>
